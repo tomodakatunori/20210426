772527f101c874136c84bed1bea9a0074be77671
tomodakatunori <hj-g20022@sist.ac.jp>
2021-05-11 12:27:58 +0900

    キーボードの入力を作った

--- keyboard.h
+++ keyboard.h
@@ -8,23 +8,20 @@
 //マクロ定義
 #define KEY_KIND_MAX 256	//取得するキーの種類	
 
-//構造体
-
 //キーボード構造体
-
 struct KEYBOARD
 {
-	char TempKeyState{ KEY_KIND_MAX };	//入力状態を管理
-	int AllKeyState{ KEY_KIND_MAX };	//すべてのキーの入力時間を管理
-	int OldKeyState{ KEY_KIND_MAX };	//以前のすべてのキーの入力時間を管理
+	char TempKeyState[ KEY_KIND_MAX ];	//入力状態を管理
+	int AllKeyState[ KEY_KIND_MAX ];	//すべてのキーの入力時間を管理
+	int OldKeyState[ KEY_KIND_MAX ];	//以前のすべてのキーの入力時間を管理
 };
 
 //外部のグローバル変数
 extern KEYBOARD keyboard;
 
 //プロトタイプ宣言
-extern VOID MyAllKeyUpdate(VOID);	//すべてのキーの状態を取得する
-extern BOOL KeyDown(int KEY_INPUT);	//特定のキーを押しているか？
-extern BOOL KeyUp(int KEY_INPUT);	//特定のキーをあげているか？
-extern BOOL KeyClick(int KEY_INPUT);	//特定のキーをクリックしたか？
-extern BOOL KeyDownKeep(int KEY_INPUT);	//特定のキーを指定のミリ秒分押し続けているか？
+extern VOID AllKeyUpdate(VOID);		//すべてのキーの状態を取得する
+extern BOOL KeyDown(int KEY_INPUT_);		//特定のキーを押しているか？
+extern BOOL KeyUp(int KEY_INPUT_);		//特定のキーをあげているか？
+extern BOOL KeyClick(int KEY_INPUT_);	//特定のキーをクリックしたか？
+extern BOOL KeyDownKeep(int KEY_INPUT_,int millSec);	//特定のキーを指定のミリ秒分押し続けているか？
--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 //ヘッダーファイルの読み込み
 #include "DxLib.h"	//DxLibを使うのに必要
 
+#include "keyboard.h"	//キーボードを使うのに必要
+
 //マクロ定義
 #define GAME_TITLE "ゲームタイトル"//ゲームタイトル
-#define GAME_WIDTH 1280	//ゲーム画面の幅
-#define GAME_HEIGTH 720	//ゲーム画面の高さ
-#define GAME_COLOR 32	//ゲームの色域
+#define GAME_WIDTH 1280		//ゲーム画面の幅
+#define GAME_HEIGTH 720		//ゲーム画面の高さ
+#define GAME_COLOR 32		//ゲームの色域
 #define GAME_ICON_ID 333	//ゲームのアイコン
-#define GAME_WINDOWBER 0 //ウィンドウバーの種類
+#define GAME_WINDOWBER 0	//ウィンドウバーの種類
 
 // プログラムは WinMain から始まります
 //windousのプログラミング＝winAPIで動いている
@@ -17,25 +19,31 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	LPSTR lpCmdLine, 
 	int nCmdShow)
 {
-	SetOutApplicationLogValidFlag(FALSE);	//Log.txtを表示しない
-	ChangeWindowMode(TRUE);	//ウィンドウモードに設定
-	SetMainWindowText(GAME_TITLE);	//ウィンドウのタイトル文字
+	SetOutApplicationLogValidFlag(FALSE);				//Log.txtを表示しない
+	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
+	SetMainWindowText(GAME_TITLE);						//ウィンドウのタイトル文字
 	SetGraphMode(GAME_WIDTH, GAME_HEIGTH, GAME_COLOR);	//ウィンドウの解像度
-	SetWindowSize(GAME_WIDTH, GAME_HEIGTH);	//ウィンドウの大きさ
-	SetBackgroundColor(255, 255, 255);	//背景の色
-	SetWindowIconID(GAME_ICON_ID);	//ウィンドウのアイコン
-	SetWindowStyleMode(GAME_WINDOWBER);	//ウィンドウバーの状態
-	SetWaitVSyncFlag(TRUE);	//ディスプレイの垂直同期の有効
-	SetAlwaysRunFlag(TRUE);	//ウィンドウをずっとアクティブにする	
+	SetWindowSize(GAME_WIDTH, GAME_HEIGTH);				//ウィンドウの大きさ
+	SetBackgroundColor(255, 255, 255);					//背景の色
+	SetWindowIconID(GAME_ICON_ID);						//ウィンドウのアイコン
+	SetWindowStyleMode(GAME_WINDOWBER);					//ウィンドウバーの状態
+	SetWaitVSyncFlag(TRUE);								//ディスプレイの垂直同期の有効
+	SetAlwaysRunFlag(TRUE);								//ウィンドウをずっとアクティブにする	
 	//こいつらはウィンドウの初期化の上に書くべき
 
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
-		return -1;			// エラーが起きたら直ちに終了
+		return -1;				// エラーが起きたら直ちに終了
 	}
 
 	//ダブルバッファリングの有効化
 	SetDrawScreen(DX_SCREEN_BACK);
+	
+	//円の中心点
+	int X = GAME_WIDTH / 2;
+	int Y = GAME_HEIGTH / 2;
+	//円の半径
+	int radius = 50;
 
 	//無限ループ
 	while (1)
@@ -43,7 +51,11 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 		if (ProcessMessage() != 0){break;}	//メッセージを受け取り続ける
 		if (ClearDrawScreen() != 0){break;} //画面の商経
+
+		//キーボードの入力の更新
+		AllKeyUpdate();
 		
+		DrawCircle(X, Y, radius, GetColor(255, 255, 0), TRUE);
 
 		ScreenFlip();	//ダブルバッファリングした画面を描画
 
@@ -51,5 +63,5 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	DxLib_End();	//ＤＸライブラリ使用の終了処理
 
-	return 0;	//ソフトの終了 
+	return 0;		//ソフトの終了 
 }
